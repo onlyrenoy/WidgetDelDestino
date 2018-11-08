6ebb888a869e7772824420d42563137c3e6b9ee1
Renoy Rahman Chowdhury <renoy.chowdhury@nttdata.com>
2018-11-08 14:22:44 +0100

    +primoCommit

---
+++ WidgetDelDestino/WidgetDelDestino/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WidgetDelDestino
+//
+//  Created by Renoy Rahman Chowdhury on 08/11/2018.
+//  Copyright © 2018 Renoy Rahman Chowdhury. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
